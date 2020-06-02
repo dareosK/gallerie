7aa47e474756c0ce9f67b0d9eeb901fadc32774e
Dareos Khalili <khalilidareos@gmail.com>
2020-06-02 15:23:23 +0200

    adds new show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -6,9 +6,18 @@ class ShowsController < ApplicationController
   end
 
   def new
+    @show = Show.new # make it available for the view
   end
 
   def create
+    @show = Show.new(show_params)
+    @user = current_user # to allow current user to create
+    @show.save
+    if @show.save!
+      redirect_to show_path(@show)
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -18,5 +27,11 @@ class ShowsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:title, :statement)
   end
 end
