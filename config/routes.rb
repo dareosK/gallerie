00044a246a7b362737b7360158b021a48d24581b
Dareos Khalili <khalilidareos@gmail.com>
2020-06-02 14:23:23 +0200

    creates shows controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  get 'shows/index'
+  get 'shows/show'
+  get 'shows/new'
+  get 'shows/create'
+  get 'shows/edit'
+  get 'shows/update'
+  get 'shows/destroy'
   devise_for :users
   root to: 'pages#home'
-  resources :shows, only: [:index, :show, :new, :create, :edit, :update ] do
+  resources :shows do
     resources :artworks, only: [:new, :create, :destroy]
   end
-  delete "shows/:id", to "shows#destroy", as: :destroy_shows
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: :show
-  resources :writings, only: [:index, :show, :new, :create, :edit, :update]
-  delete "writings/:id", to "writings#destroy", as: :destroy_writings
+  resources :writings
+
 end
