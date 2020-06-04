a772d28682f12c51cf175d4572d3cf7a657424d0
Dareos Khalili <khalilidareos@gmail.com>
2020-06-04 23:12:06 +0200

    adds seeds : Show one done, Shows 2 and 3 dont have panels and artworks

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,14 @@ Show.destroy_all
 Writing.destroy_all
 User.destroy_all
 
-user_one = User.create!(
-    email: "rinaldi@icloud.com",
-    password: "Secret",
-    name: "Marina Rinaldi", 
-    bio: "Hi I am Marina Rinaldi, from Milano. I have stidued at 'Kunstakademie Düsseldorf' under Joseph Beuys. He taught us the power of art. This virtual gallery helps me spred my work and fight for a better world."
+
+# ========== #
+# USER SEEDS #
+# ========== #
+
+mike = User.create!(
+    email: "emoellervon@icloud.com",
+    password: "Secret"
   )
 puts "First"
 
@@ -27,145 +30,222 @@ oscar = User.create!(
   password: "Secret"
   )
 
+# ============= #
+# WRITING SEEDS #
+# ============= #
 
-Writing.create!(
+Writing.create!([
+  {
     title: "Art Article",
     publishing_date: DateTime.new(2020,9,14),
     publisher: "New York Times",
     article: "This is a very short article that is just placed here as a placeholder",
-    user_id: oscar.id
-  )
-
-Writing.create!(
+    user_id: User.first.id
+  },
+  {
     title: "Imagine a magic Tree",
     publishing_date: DateTime.parse("25-09-2020"),
     publisher: "Arts Space",
     article: "And then suddenly someone walked thru the forest and stood in front of this giant tree... A Kauri",
-    user_id: oscar.id
-  )
-
+    user_id: User.first.id
+  }])
 puts "Second"
+
 Writing.create!([
   {
     title: "Creative Mushroom",
     publishing_date: DateTime.new(2020,9,14),
     publisher: "Geek Art",
     article: "Do you know Alice and the wonderland? Alice sits on a riverbank on a warm summer day, drowsily reading over her sister’s shoulder, when she catches sight of a White Rabbit in a waistcoat running by her. The White Rabbit pulls out a pocket watch, exclaims that he is late, and pops down a rabbit hole. Alice follows the White Rabbit down the hole and comes upon a great hallway lined with doors. She finds a small door that she opens using a key she discovers on a nearby table. Through the door, she sees a beautiful garden, and Alice begins to cry when she realizes she cannot fit through the door. She finds a bottle marked “DRINK ME” and downs the contents. She shrinks down to the right size to enter the door but cannot enter since she has left the key on the tabletop above her head. Alice discovers a cake marked “EAT ME” which causes her to grow to an inordinately large height. Still unable to enter the garden, Alice begins to cry again, and her giant tears form a pool at her feet. As she cries, Alice shrinks and falls into the pool of tears. The pool of tears becomes a sea, and as she treads water she meets a Mouse. The Mouse accompanies Alice to shore, where a number of animals stand gathered on a bank. After a “Caucus Race,” Alice scares the animals away with tales of her cat, Dinah, and finds herself alone again.",
-    user_id: user_one.id
+    user_id: oscar.id
   },
   {
     title: "Imagine a magic Tree",
     publishing_date: DateTime.parse("25-09-2020"),
     publisher: "Arts Space",
     article: "The Wood That Fights Fire... As gold-crazed settlers flocked to the Bay Area in the 19th century, they needed lumber to build their homes and mines. And what better way to get a lot of wood at once than toppling an enormous redwood? Little did they know the wood had a greater legacy in store. In April 1906, San Francisco awoke to a major earthquake. As if the rocking buildings weren’t bad enough, residents were soon battling massive fires that threatened to consume the city. But the city didn’t go up in smoke for one reason: redwoods. Though the fire spread for three days, a curious thing happened when it hit a building made from the mighty tree. While not totally fireproof, the redwood’s low resin content and porous grain allow it to take on moisture, which makes it far more flame-resistant than woods like pine. As one reporter noted, “[I]n all principal directions, the fire was finally stopped in the very midst of frame redwood buildings.” ",
-    user_id: user_one.id
+    user_id: oscar.id
   }])
 puts "seeding of stories done"
 
-file = URI.open("https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/16077575318379.5c49230e91b9d.jpg")
+# # ========== #
+# # SHOW SEEDS #
+# # note: each Show needs Panels. Find the iteration for the Panel seeds
+# # on the ARTWORK SEEDS section.
+# # ========== #
+
+# Show 1
+show_one_img = URI.open("https://photos.lensculture.com/original/963c7429-9d8d-4d4a-b8e9-f5f6f5fcfa61.jpg")
 show_one = Show.new(
-    title: "A Maze Ing",
-    statement: "Sometimes I am locked in your own walls. Mentally. Physically. What does this do to us? ",
-    user_id: user_one.id
+    title: "Alec Soth - I Know How Furiously Your Heart is Beating",
+    statement: "After abandoning portraiture for an entire year, Alec Soth returns to the genre with a new photobook, emphasizing the importance of collaboration while photographing his subjects.
+The very first image in Alec Soth’s new book, I Know How Furiously Your Heart Is Beating, reminds me of a time when I was five years old, and I convinced my best friend to let her parakeet out of its cage. In Soth’s photograph titled “Cammy’s View. Salt Lake City.” a cockatiel is perched at a closed window. Its head tilts back to look at the camera. Although the animal isn’t in a cage, it is in a corner and the trees that are visible through the window panes are too far away to be in focus. This scene makes me think of my childhood friend and her bird, because that bird was also drawn towards windows. It didn’t seem to know the difference between glass and air. I can remember watching the little animal as it searched for a way out, immediately knowing that I had made a mistake. When we finally managed to catch the parakeet, its entire body was like a pulsing heart. At that age, I was still negotiating the difference between kindness and cruelty, but with the frightened animal in my fist, I felt that the freedom I negotiated for it was no favor. I knew that, rather than concern for the animal, it had been my own curiosity that prompted all this excitement.",
+    user_id: oscar.id
 )
-show_one.photo.attach(io: file, filename: 'art.jpg', content_type: 'image/jpg')
+show_one.photo.attach(io: show_one_img, filename: '963c7429-9d8d-4d4a-b8e9-f5f6f5fcfa61.jpg', content_type: 'image/jpg')
 show_one.save!
+puts "Alec Soth Show created"
+
+# Show 2
+show_two_img = URI.open("https://news.artnet.com/app/news-upload/2017/06/2004-138-Gedser_A4_440x300mm_LAC-697x1024.jpg")
+show_two = Show.new(
+    title: "Wolfgang Tilmans : Retrospective",
+    statement: "This year's big summer exhibition is devoted to the artist Wolfgang Tillmans. It is the first comprehensive engagement with the medium of photography at the Fondation Beyeler, which some time ago added a wonderful group of works by Tillmans to its collection. Around 200 photographic works dating from 1989 to 2017 will be on show from May 28 to October 1, together with a new audiovisual installation.
+Tillmans first made a name for himself in the early 1990s through photographs that have attained iconic status for their evocation of the mood of an entire generation, with its carefree urge for freedom and its desire to seize life’s moments. Soon, however, he widened his focus, experimenting with the means of photography to develop a new visual language. He created his images with and without a camera and also using a photocopier. In addition to traditional genres such as portrait, still life and landscape, the exhibition presents abstract works that play with the limits of the visible. It will show how Tillman’s work is concerned with the creation of images rather than with photography in the conventional sense. The exhibition is being designed in close cooperation with the artist.",
+    user_id: oscar.id
+)
+show_two.photo.attach(io: show_two_img, filename: '2004-138-Gedser_A4_440x300mm_LAC-697x1024.jpg', content_type: 'image/jpg')
+show_two.save!
+puts "Wolfgang Tillmans Show created"
+
+# Show 3
+show_three_img = URI.open("https://d32dm0rphc51dk.cloudfront.net/ntbzf-5vKx_ZZpku5tl1mw/large.jpg")
+show_three = Show.new(
+    title: "Igor Moritz : Untitled Self",
+    statement: "Igor Moritz was born in Poland in 1996. At a young age, he and his family were forced to relocate to London, but when he was old enough, he returned to his home, Lubin, to pursue his studies in fine arts. Moritz’s artistic energy flows out him so intensely; constantly creating with an almost Egon Schieleian-like passion, Moritz’s muse is the world around him.
+Those in his most intimate circles are the ones he portrays, giving the viewer a view into Moritz’s world. His paintings and drawing flirt with mimesis while breaking all rules of perspective, proportion and realism. He morphs and composes the perspective, space and figures in a way, in order to create visual tension.
+Color is something that is always at the forefront of his works, as Moritz sees color to be a way to tap into the viewer’s visual palette. The effect are artworks that are palpable, emotional, and visceral.",
+    user_id: oscar.id
+)
+show_three.photo.attach(io: show_three_img, filename: 'large.jpg', content_type: 'image/jpg')
+show_three.save!
+puts "Igor Moritz Show created"
 
 
-6.times do
-    Panel.create!([
-    show_id: Show.first.id
-  ])
+
+# # ============= #
+# # ARTWORK SEEDS #
+# # ============= #
+
+# Artworks Show 1
+4.times do
+   Panel.create!([
+   show_id: Show.first.id
+ ])
 end
 puts "Panels for first show created"
 
-# mpdg = URI.open("https://s.studiobinder.com/wp-content/uploads/2019/12/Manic-Pixie-Dream-Girl-Featured.jpg")
-
-mpdg = URI.open("https://www.pigasus-shop.de/images/product_images/popup_images/7452_0.jpg")
-
-show_two = Show.new(
-  title: "Real Art By Real Guys",
-  statement: "My ex-girlfriend was very inspiring- a muse, you might say. I'm not going to mention her name at all!",
-  user_id: user_one.id
+show_one_artwork_one_img = URI.open("https://photos.lensculture.com/original/963c7429-9d8d-4d4a-b8e9-f5f6f5fcfa61.jpg")
+show_one_artwork_one = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_two.photo.attach(io: mpdg, filename: 'mpdg.jpg', content_type: 'image/jpg')
-show_two.save!
+show_one_artwork_one.photo.attach(io: show_one_artwork_one_img, filename: '963c7429-9d8d-4d4a-b8e9-f5f6f5fcfa61.jpg', content_type: 'image/jpg')
+show_one_artwork_one.save!
 
-dualism = URI.open("https://product-image.juniqe-production.juniqe.com/media/catalog/product/seo-cache/x800/38/13/38-13-202P-Black/Dualism-Genis-Carreras-Poster-im-Kunststoffrahmen.jpg")
-
-show_five = Show.new(
-  title: "Dualism, Bauhaus, Shapes",
-  statement: "You might say design rules the world. That is true in some ways, good clear minimalist design ruels the world",
-  user_id: user_one.id
+show_one_artwork_two_img = URI.open("https://photos.lensculture.com/original/e278964e-f804-42fb-b560-c81dcb22a36a.jpg")
+show_one_artwork_two = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_five.photo.attach(io: dualism, filename: 'mpdg.jpg', content_type: 'image/jpg')
-show_five.save!
+show_one_artwork_two.photo.attach(io: show_one_artwork_two_img, filename: 'e278964e-f804-42fb-b560-c81dcb22a36a.jpg', content_type: 'image/jpg')
+show_one_artwork_two.save!
 
-mick = URI.open("https://www.artistsposters.com/images/product_images/popup_images/19630.jpg")
-show_six = Show.new(
-  title: "Dualism, Bauhaus, Shapes",
-  statement: "You might say design rules the world. That is true in some ways, good clear minimalist design ruels the world",
-  user_id: user_one.id
+show_one_artwork_three_img = URI.open("https://photos.lensculture.com/original/10054579-e3a4-49b0-8c68-6f7a6b964088.jpg")
+show_one_artwork_three = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_six.photo.attach(io: mick, filename: 'mpdg.jpg', content_type: 'image/jpg')
-show_six.save!
+show_one_artwork_three.photo.attach(io: show_one_artwork_three_img, filename: '10054579-e3a4-49b0-8c68-6f7a6b964088.jpg', content_type: 'image/jpg')
+show_one_artwork_three.save!
 
-moro = URI.open("https://i.pinimg.com/originals/87/e5/76/87e576829a9b2784c27f6fc8144f0240.png")
-show_seven = Show.new(
-  title: "Dualism, Bauhaus, Shapes",
-  statement: "You might say design rules the world. That is true in some ways, good clear minimalist design ruels the world",
-  user_id: user_one.id
+show_one_artwork_four_img = URI.open("https://photos.lensculture.com/original/180ebb17-5cbb-4e0a-9f83-274d82369a90.jpg")
+show_one_artwork_four = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_seven.photo.attach(io: moro, filename: 'mpdg.jpg', content_type: 'image/jpg')
-show_seven.save!
+show_one_artwork_four.photo.attach(io: show_one_artwork_four_img, filename: '180ebb17-5cbb-4e0a-9f83-274d82369a90.jpg', content_type: 'image/jpg')
+show_one_artwork_four.save!
 
-doner = URI.open("https://www.expatica.com/app/uploads/sites/6/2014/05/Doner-kebab-1920x1080.jpg")
-show_three = Show.new(
- title: "14 Different Sandwiches I have Known and Loved",
-  statement: "Berlin-based photographer Marcus Schmitty documents his existential misery, each feeling represented by a different doner.",
-  user_id: User.last.id
+show_one_artwork_five_img = URI.open("https://photos.lensculture.com/original/e819b04f-66e4-4514-83aa-77d1f2fa4710.jpg")
+show_one_artwork_five = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_three.photo.attach(io: doner, filename: 'doner.jpg', content_type: 'image/jpg')
-show_three.save!
+show_one_artwork_five.photo.attach(io: show_one_artwork_five_img, filename: 'e819b04f-66e4-4514-83aa-77d1f2fa4710.jpg', content_type: 'image/jpg')
+show_one_artwork_five.save!
 
-duck = URI.open("https://upload.wikimedia.org/wikipedia/commons/1/14/Rubber_Duck_%288374802487%29.jpg")
-show_four = Show.new(
-  title: "Debugged",
-  statement: "This photo-story documents the intimate, destructive relationship between a man and his rubber duck.",
-  user_id: User.last.id
+show_one_artwork_six_img = URI.open("https://photos.lensculture.com/original/bc2db11c-8a44-40f5-aae0-93fe14a95c3b.jpg")
+show_one_artwork_six = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_four.photo.attach(io: duck, filename: 'duck.jpg', content_type: 'image/jpg')
-show_four.save!
+show_one_artwork_six.photo.attach(io: show_one_artwork_six_img, filename: 'bc2db11c-8a44-40f5-aae0-93fe14a95c3b.jpg', content_type: 'image/jpg')
+show_one_artwork_six.save!
 
-img_one = URI.open("https://npg.si.edu/exhibit/feature/images/schoeller_full.jpg")
-show_five = Show.new(
-  title: "Debugged",
-  statement: "This photo-story documents the intimate, destructive relationship between a man and his rubber duck.",
-  user_id: User.last.id
+show_one_artwork_seven_img = URI.open("https://photos.lensculture.com/original/64c023d1-6fc8-4a83-bc59-ed4420c129e8.jpg")
+show_one_artwork_seven = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_five.photo.attach(io: img_one, filename: 'img_one.jpg', content_type: 'image/jpg')
-show_five.save!
+show_one_artwork_seven.photo.attach(io: show_one_artwork_seven_img, filename: '64c023d1-6fc8-4a83-bc59-ed4420c129e8.jpg', content_type: 'image/jpg')
+show_one_artwork_seven.save!
 
-img_two = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Mona_Lisa-restored.jpg/1200px-Mona_Lisa-restored.jpg")
-show_six = Show.new(
-  title: "Debugged",
-  statement: "This photo-story documents the intimate, destructive relationship between a man and his rubber duck.",
-  user_id: User.last.id
+show_one_artwork_eight_img = URI.open("https://photos.lensculture.com/original/fedce6c0-6de8-4c76-a879-2a7217db5c03.jpg")
+show_one_artwork_eight = Artwork.new(
+    artist: "Alec Soth",
+    show_id: Show.first.id,
+    panel_id: Panel.first.id,
+    user_id: User.first.id,
 )
-show_six.photo.attach(io: img_two, filename: 'img_two.jpg', content_type: 'image/jpg')
-show_six.save!
+show_one_artwork_eight.photo.attach(io: show_one_artwork_eight_img, filename: 'fedce6c0-6de8-4c76-a879-2a7217db5c03.jpg', content_type: 'image/jpg')
+show_one_artwork_eight.save!
 
-show_seven = Show.new(
-  title: "Debugged",
-  statement: "This photo-story documents the intimate, destructive relationship between a man and his rubber duck.",
-  user_id: User.last.id
-)
-show_seven.photo.attach(io: img_one, filename: 'img_one.jpg', content_type: 'image/jpg')
-show_seven.save!
+#### PLACEHOLDER FOR SHOW_ONE ARTWORK INSTANCE ####
 
-6.times do
-    Panel.create!([
-    show_id: Show.last.id
-  ])
-end
-puts "Panels for second show created"
+# show_one_artwork_one_img = URI.open("# Fill This")
+# artwork_one = Artwork.new(
+    # artist: "Alec Soth",
+    # show_id: Show.first.id,
+    # panel_id: Panel.first.id,
+    # user_id: User.first.id,
+# )
+# artwork_one.photo.attach(io: show_one_artwork_one_img, filename: '# Fill This', content_type: 'image/jpg')
+# artwork_one.save!
+puts "Alec Soth show artworks done"
+
+
+
+#### PLACEHOLDER FOR SHOW_TWO ARTWORK INSTANCES ####
+
+# Artworks Show 2
+
+# show_two_artwork_one_img = URI.open("# Fill This")
+# show_two_artwork_one = Artwork.new(
+#     artist: "Wolfgang Tillmans",
+#     show_id: # Fill This,
+#     panel_id: # Fill This,
+#     user_id: User.first.id,
+# )
+# show_two_artwork_one.photo.attach(io: show_two_artwork_one_img, filename: '# Fill This', content_type: 'image/jpg')
+# show_two_artwork_one.save!
+
+
+
+#### PLACEHOLDER FOR SHOW_THREE ARTWORK INSTANCES ####
+
+# Artworks Show 3
+
+# show_three_artwork_one_img = URI.open("# Fill This")
+# show_three_artwork_one = Artwork.new(
+#     artist: "Igor Moritz",
+#     show_id: # Fill This,
+#     panel_id: # Fill This,
+#     user_id: User.first.id,
+# )
+# show_three_artwork_one.photo.attach(io: show_three_artwork_one_img, filename: '# Fill This', content_type: 'image/jpg')
+# show_three_artwork_one.save!
