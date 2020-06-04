c59230da3469b7be4cb316fdd4b5afd7c2a3e5c6
Ella Moeller <emoellervon@icloud.com>
2020-06-04 16:01:35 +0200

    dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_133130) do
-
+ActiveRecord::Schema.define(version: 2020_06_04_102126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_133130) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
