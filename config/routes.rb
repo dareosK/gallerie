8aca443b8ecc56217ae6922fa7897dacd1ebc64c
Kat mcg <cathaumann@gmail.com>
2020-06-08 12:23:03 +0200

    changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "dashboard#show"
   resources :shows, only: [:show, :index, :create, :update, :destroy, :edit] do
-    resources :panels, only: [:create, :index, :show, :update, :destroy] do
-      resources :artworks, only: [:create, :update, :destroy]
-    end
+    resources :panels, only: [:create, :index, :show, :update, :destroy]
+  end
+
+  resources :panels, only: [:create, :index, :show, :update, :destroy] do
+    resources :artworks, only: [:create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: :show
