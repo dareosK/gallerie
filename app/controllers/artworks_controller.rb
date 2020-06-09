e0de2285bfb51977502b7b432ad7542a30fb7462
Kat mcg <cathaumann@gmail.com>
2020-06-09 19:10:50 +0200

    add artwork functionality

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,12 +1,29 @@
 class ArtworksController < ApplicationController
 
   def new
+    
   end
 
   def create 
+    @panel = Panel.find(params[:panel_id])
+    @show = @panel.show
+    @artwork = Artwork.new(art_params)
+    @artwork.panel = @panel
+    @artwork.user = current_user
+    @artwork.show = @show
+    if @artwork.save!
+      redirect_to edit_show_path(@show)
+    end
+    authorize @artwork
   end
 
   def destroy
+  end
+
+    private
+
+  def art_params
+    params.require(:artwork).permit(:title, :description, :photo, :artist)
   end
   
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,5 +1,6 @@
 class Artwork < ApplicationRecord
   belongs_to :user
   belongs_to :panel
+  belongs_to :show
   has_one_attached :photo
 end
