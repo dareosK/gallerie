d151e82ae3e93895ceab56a79e927a7d084c95cb
Dareos Khalili <khalilidareos@gmail.com>
2020-06-11 15:25:20 +0200

    carousel done

--- app/javascript/components/clickable.js
+++ app/javascript/components/clickable.js
@@ -1,16 +1,17 @@
 const clickable = () => {
 	const tab1 = document.querySelector("#home-tab");
 	const tab2 = document.querySelector("#profile-tab");
+  if (tab1 || tab2) {
+    tab1.addEventListener("click",(event) => {
+    tab2.style.backgroundColor = 'white';
+    event.currentTarget.style.backgroundColor = 'lightgray';
+  });
 
-	tab1.addEventListener("click",(event) => {
-		tab2.style.backgroundColor = 'white';
-		event.currentTarget.style.backgroundColor = 'lightgray';
-	});
-
-	tab2.addEventListener("click",(event) => {
-		tab1.style.backgroundColor = 'white';
-		event.currentTarget.style.backgroundColor = 'lightgray';
-	});
+  tab2.addEventListener("click",(event) => {
+    tab1.style.backgroundColor = 'white';
+    event.currentTarget.style.backgroundColor = 'lightgray';
+  });
+  }
 }
 
 export { clickable };
