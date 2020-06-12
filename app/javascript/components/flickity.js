f2b22cbd3df29228a9c7883a1e4224ed7caca33f
Dareos Khalili <khalilidareos@gmail.com>
2020-06-12 10:51:58 +0200

    harden JS

--- app/javascript/components/flickity.js
+++ app/javascript/components/flickity.js
@@ -3,14 +3,16 @@ import Flickity from 'flickity';
 
 const initFlickity = () => {
   var elem = document.querySelector('.flick-carousel');
-  var flkty = new Flickity( elem, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    freeScroll: true,
-    autoPlay: true,
-    draggable: true,
-  });
+  if (elem) {
+    var flkty = new Flickity( elem, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      freeScroll: true,
+      autoPlay: true,
+      draggable: true,
+    });
+  }
 }
 
 export { initFlickity }
