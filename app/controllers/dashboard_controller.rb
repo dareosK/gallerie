e74c1b27d9de17fd61bc541edb9270bfeb59254f
Dareos Khalili <khalilidareos@gmail.com>
2020-08-18 11:00:01 +0200

    adds comments on the code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,14 @@
 class DashboardController < ApplicationController
-skip_before_action :authenticate_user!, only: [ :show ]
-	def show 
-		# @current_user = current_user
-		@user = User.find(params[:user_id])
-		@writing = @user.writings
-		@shows = @user.shows
-		authorize @shows
-		authorize @writing
-		@show = Show.new
-		# @shows = policy_scope(Show).order(created_at: :desc)
-	end 
+skip_before_action :authenticate_user!, only: [:show]
+
+  def show
+    # @current_user = current_user
+    @user = User.find(params[:user_id])
+    @writing = @user.writings
+    @shows = @user.shows
+    authorize @shows
+    authorize @writing
+    @show = Show.new
+    # @shows = policy_scope(Show).order(created_at: :desc)
+  end
 end
